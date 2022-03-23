0022085466b427e3b9a25623201337cef446b888
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-03-23 02:01:04 +0000

    Remove examples

--- examples/examples.h
+++
@@ -1,7 +0,0 @@
-/// All opengl brogl examples
-#ifndef _EXAMPLES_H_
-#define _EXAMPLES_H_
-
-#include "texturing/texturing.h"
-
-#endif // !_EXAMPLES_H_
--- examples/texturing/shader.frag
+++
@@ -1,9 +0,0 @@
-varying vec2 vTex;
-
-uniform sampler2D uTex;
-
-void main()
-{
-	vec4 tex = texture2D(uTex, vTex);
-	gl_FragColor = tex;
-}
