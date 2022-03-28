3c122851ff8feb6a1080aff11c1fd313c6b36acf
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-03-28 01:47:31 +0100

    Add axis support

--- brogl_game/_inputmanager.h
+++ brogl_game/_inputmanager.h
@@ -4,9 +4,12 @@
 
 // External dependencies
 #include <SDL2/SDL.h>
+#include <GLM/glm.hpp>
 
 // STL dependencies
 #include <unordered_map>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 namespace bro
@@ -20,14 +23,26 @@ namespace bro
 		// SDL handle to key states
 		const unsigned char* keyStates{ nullptr };
 		int keyStateSize{ 0 };
-
 		// Key state mask for changed keys
 		unsigned char* keyMask{ nullptr };
 
 		// Mouse coordinates
 		int mouseX{ 0 }, mouseY{ 0 };
-		// Mouse state
+		// Mouse state for buttons
 		int mouseState{ 0 };
+		// Mouse mask for changed buttons
+		int mouseMask{ 0 };
+
+		// Button axes allow for a range between -1 and 1
+		struct axis
+		{
+			// Positive and negative key scancodes to check
+			SDL_Scancode posKey, negKey;
+		};
+		// Axis list 
+		std::vector<axis> buttonAxes;
+		// Indices map to retreive index for specific axis
+		std::unordered_map<std::string, size_t> axesIndices;
 
 	private: // Internal engine methods
 		friend engine;
@@ -43,9 +58,20 @@ namespace bro
 
 		bool GetKeyUp(const SDL_Scancode& _scanCode);
 
+		bool GetMouseButton(const int& _button);
 
+		bool GetMouseButtonDown(const int& _button);
+
+		bool GetMouseButtonUp(const int& _button);
+
+		glm::vec3 GetMousePosition() { return { mouseX, mouseY, 0 }; };
+
+		size_t GetAxisIndex(const char* _axisName);
 
 		float GetAxis(const char* _axisName);
+		float GetAxis(const size_t& _axisIndex);
+
+		void AddAxis(const char* _axisName, const SDL_Scancode& _positiveKey, const SDL_Scancode& _negativeKey);
 
 	public:
 		_inputmanager();
--- brogl_game/implement/_inputmanager.cpp
+++ brogl_game/implement/_inputmanager.cpp
@@ -5,13 +5,16 @@ namespace bro
 
 	void _inputmanager::EnginePreUpdate()
 	{
-		mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+		// Copy over key states from previous frame
+		for (int i = 0; i < keyStateSize; ++i) { keyMask[i] = keyStates[i]; }
+		// Copy over mouse states from previous frame
+		mouseMask = mouseState;
 	}
 
 	void _inputmanager::EnginePostUpdate()
 	{
-		// Copy over states from previous frame
-		for (int i = 0; i < keyStateSize; ++i) { keyMask[i] = keyStates[i]; }
+		// Capture mouse state and position
+		mouseState = SDL_GetMouseState(&mouseX, &mouseY);
 	}
 
 	bool _inputmanager::GetKey(const SDL_Scancode& _scanCode)
@@ -29,9 +32,44 @@ namespace bro
 		return !keyStates[_scanCode] && keyMask[_scanCode];
 	}
 
+	bool _inputmanager::GetMouseButton(const int& _button)
+	{
+		return (mouseState & SDL_BUTTON(_button)) != 0;
+	}
+
+	bool _inputmanager::GetMouseButtonDown(const int& _button)
+	{
+		return (mouseState & SDL_BUTTON(_button)) != 0 && (mouseMask & SDL_BUTTON(_button)) == 0;
+	}
+
+	bool _inputmanager::GetMouseButtonUp(const int& _button)
+	{
+		return (mouseState & SDL_BUTTON(_button)) == 0 && (mouseMask & SDL_BUTTON(_button)) != 0;
+	}
+
+	size_t _inputmanager::GetAxisIndex(const char* _axisName)
+	{
+		std::unordered_map<std::string, size_t>::iterator it = axesIndices.find(_axisName);
+		if (it == axesIndices.end()) { throw std::runtime_error("Could not find axis name"); }
+		return it->second;
+	}
+
 	float _inputmanager::GetAxis(const char* _axisName)
 	{
-		return 0;
+		return GetAxis(GetAxisIndex(_axisName));
+	}
+	float _inputmanager::GetAxis(const size_t& _axisIndex)
+	{
+		const axis& ax = buttonAxes[_axisIndex];
+		// Test scancode buttons
+		float d = GetKey(ax.posKey) - GetKey(ax.negKey);
+		return d;
+	}
+
+	void _inputmanager::AddAxis(const char* _axisName, const SDL_Scancode& _positiveKey, const SDL_Scancode& _negativeKey)
+	{
+		buttonAxes.push_back({_positiveKey, _negativeKey});
+		axesIndices[_axisName] = buttonAxes.size() - 1;
 	}
 
 	_inputmanager::_inputmanager()
