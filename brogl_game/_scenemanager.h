1d381ac60302e04dd5f256a8e26a92eb1d2e8c51
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-03-28 01:48:42 +0100

    Add documentation

--- brogl_game/_scenemanager.h
+++ brogl_game/_scenemanager.h
@@ -39,9 +39,12 @@ namespace bro
 		/// @param _scene Reference to scene
 		void AddScene(scene& _scene);
 
-		/// @brief 
-		/// @param _name 
+		/// @brief Get current scene handle
+		/// @return Handle to current scene
 		scene* GetCurrentScene() { return *currentScene; }
+
+		/// @brief Initialize manager on engine start up
+		void EngineStart();
 
 	public:
 		/// @brief Go to next immediate scene handle
@@ -57,19 +60,33 @@ namespace bro
 		/// @param _sceneName Name of the scene
 		void GotoScene(const char* _sceneName);
 
+		/// @brief Set scene indices
+		/// @param _indices New indices to set 
 		void SetSceneIndices(std::vector<size_t> _indices);
+		/// @brief Set scene indices
+		/// @param _sceneNames Scene names to get indices from
 		void SetSceneIndices(std::vector<const char*> _sceneNames);
 
+		/// @brief Push scene index to indices list
+		/// @param _index Index of scene
 		void AddSceneIndex(size_t _index);
+		/// @brief Push scene index to indices list
+		/// @param _sceneName Scene name to get index from
 		void AddSceneIndex(const char* _sceneName);
 
+		/// @brief Find the index of a scene from the scene manager
+		/// @param _sceneName Name of the scene
+		/// @return Index of scene
+		size_t FindSceneIndex(const char* _sceneName);
+
+		/// @brief Go to next scene handle from indices list
 		void NextScene();
+		/// @brief Go to previous scene handle from indices list
 		void PreviousScene();
 
-		void ResetSceneIndex();
+		/// @brief Reset to the beginning of the indices list, doesn't change the current scene
+		void ResetSceneIndices();
 
-	public:
-		_scenemanager();
 	};
 }
 
