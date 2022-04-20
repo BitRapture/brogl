6fd8e1b1dd38ccbae23a04ce6edc27a3ff4555d7
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-04-20 21:45:40 +0100

    Change resource layout

--- brogl_game/_objectmanager.h
+++ brogl_game/_objectmanager.h
@@ -2,12 +2,28 @@
 #ifndef _OBJECTMANAGER_H_
 #define _OBJECTMANAGER_H_
 
+// Local dependencies
+#include "../brogl/brogl.h"
+
+// STL dependencies
+#include <vector>
+#include <unordered_map>
+#include <string>
+
 namespace bro
 {
 	class gameobject;
+	class component;
 
 	class _objectmanager
 	{
+	private:
+		// Map of global resources
+		std::unordered_map<std::string, glhandle*> resources;
+
+	public:
+		void AddResource(const char* _name, glhandle& _resource) { resources[_name] = &_resource; }
+		glhandle* GetResource(const char* _name) { return resources[_name]; }
 
 	};
 
